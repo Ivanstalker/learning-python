89698f240b9a08b327253686dac6e0bf034f9a2e
ivan stalker <kiv_krs@mail.ru>
2024-12-08 18:10:45 +0200

    homework

---
+++ ..py
@@ -0,0 +1,18 @@
+
+
+
+s = "hello asd hello hello"
+s = s.replace('a', "b")
+print(s)
+print(s[6])
+
+
+l2 = '5'.join('35')
+print(l2)
+
+l1 = [1, 2, 3]
+l2 = [3, 4, 7, 93, 3, -1]
+l1.pop()
+l1.extend(l2)
+l1.sort()
+print(l1.clear())
